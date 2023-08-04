231f3797733ada68588655781761f29b46146cb2
Edward Angeles <edward.angelesm@gmail.com>
2023-08-04 10:29:19 +0100

    Code with setState

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,16 +1,24 @@
 import { useState } from "react";
 
 export const CounterApp = () => {
-  const [counter, setCounter] = useState(10);
+  const [counters, setCounters] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
 
   return (
     <>
-      <h1>Counter: {counter}</h1>
+      <h1>Counter1: {counters.counter1}</h1>
+      <h1>Counter2: {counters.counter2}</h1>
+      <h1>Counter3: {counters.counter3}</h1>
 
       <hr />
 
       <button
-        onClick={() => setCounter(counter + 1)}
+        onClick={() =>
+          setCounters({ ...counters, counter1: counters.counter1 + 1 })
+        }
         className="btn btn-primary"
       >
         +1
