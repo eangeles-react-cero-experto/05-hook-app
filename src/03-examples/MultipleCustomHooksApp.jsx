ad75f615ac32bfb68a295e1da4082829cedac095
Edward Angeles <edward.angelesm@gmail.com>
2023-08-12 12:38:26 +0100

    Refactor MultipleCustomHooksApp

--- src/03-examples/MultipleCustomHooksApp.jsx
+++ src/03-examples/MultipleCustomHooksApp.jsx
@@ -4,14 +4,14 @@ import { LoadingAlert, Quote } from ".";
 export const MultipleCustomHooksApp = () => {
   const { counter, incrementCounter } = useCounter();
   const { data, isLoading, error } = useFetch(
-    `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`
+    `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`,
   );
 
   const { quote, author } = !!data && data[0];
 
   return (
     <>
-      <h1>BreakinBad Quotes</h1>
+      <h1>BreakingBad Quotes</h1>
       <hr />
 
       {isLoading ? <LoadingAlert /> : <Quote quote={quote} author={author} />}
